1f05349e143becc15c4cf39fd045efbbacfa6c4d
Ilya <ilya.polonskiy.00@example.com>
2024-05-30 18:25:08 +0300

    Добавление моделей

--- models.py
+++ models.py
@@ -1,6 +1,28 @@
-from _sqlite3 import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-db = SQLAlchemy
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
-class Users():
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class ResortCity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<ResortCity {self.name}>'
+
+class TransportType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<TransportType {self.name}>'
